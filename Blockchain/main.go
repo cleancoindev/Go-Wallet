5fa79b633127731c45b2cfa3e966fbe8e526ee70
sunnyRK <radadiyasunny970@gmail.com.com>
2019-09-17 09:34:27 +0530

    Add wallet module and Mux api to interact with blockchain

--- Blockchain/main.go
+++ Blockchain/main.go
@@ -4,8 +4,13 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"net/http"
+	"encoding/json"
+	"strconv"
+	"time"
 
-	wallet "github.com/sunnyradadiya/P2P/Wallet"
+	"github.com/gorilla/mux"
+	wallet "github.com/sunnyRK/GO-WALLET/Wallet"
 )
 
 type Blockchain struct {
@@ -18,7 +23,11 @@ type Block struct {
 	PrevHash         []byte
 	SenderAddress    string
 	RecipientAddress string
+	Timestamp string
 }
+
+var wallets wallet.Wallets
+var chain Blockchain
 
 func (b *Block) DeriveHash() {
 	info := bytes.Join([][]byte{b.PrevHash}, []byte{})
@@ -27,7 +36,8 @@ func (b *Block) DeriveHash() {
 }
 
 func CreateBlock(token int, prevHash []byte, senderAddress string, recipientAddress string) *Block {
-	block := &Block{[]byte{}, token, prevHash, senderAddress, recipientAddress}
+	t := time.Now()
+	block := &Block{[]byte{}, token, prevHash, senderAddress, recipientAddress, t.String()}
 	block.DeriveHash()
 	return block
 }
@@ -47,38 +57,51 @@ func InitBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{Genesis()}}
 }
 
-func main() {
-
-	wallets := &wallet.Wallets{}
-
-	for j := 0; j < 3; j++ {
-		wallets.AddWallet()
-	}
-
-	chain := InitBlockchain()
-
+func addWallet(w http.ResponseWriter, r *http.Request) {
+	// wallets := &wallet.Wallets{}
+	w.Header().Set("Content-Type", "application/json")
+	address := wallets.AddWallet()
+	_ = json.NewDecoder(r.Body).Decode(address)
 	addresses := wallet.GetAllAddresses()
-	for i := 0; i < len(addresses)-1; i++ {
-		val := 10 * (i + 1)
-		if wallets.Wallets[addresses[i]].Token >= val {
-			wallets.Wallets[addresses[i]].Token = wallets.Wallets[addresses[i]].Token - val
-			wallets.Wallets[addresses[i+1]].Token = wallets.Wallets[addresses[i+1]].Token + val
-			chain.AddBlock(val, addresses[i], addresses[i+1])
-		}
-	}
-
-	fmt.Println()
-	for _, block := range chain.blocks {
-		fmt.Printf("Token in Block: %d\n", block.Token)
-		fmt.Printf("Previous Hash: %x\n", block.PrevHash)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Printf("Sender Address: %s\n", block.SenderAddress)
-		fmt.Printf("Recipient Address: %s\n", block.RecipientAddress)
-		fmt.Println()
-	}
-
-	for address := range wallets.Wallets {
-		fmt.Println("\naddress : ", address, "\nToken : ", wallets.Wallets[address].Token)
-		fmt.Println("privatekey : ", wallets.Wallets[address].PrivateKey, "\npublickey : ", wallets.Wallets[address].PublicKey)
-	}
+	addresses = append(addresses, address)
+	json.NewEncoder(w).Encode(&address)
 }
+
+func getAllWalletAddresses(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	addresses := wallet.GetAllAddresses()
+	json.NewEncoder(w).Encode(addresses)
+}
+
+func getAllWalletDetails(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(wallets)
+}
+
+func TransferToken(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	token, _ := strconv.Atoi(vars["val"])
+	sender := vars["sender"]
+	recipient := vars["recipient"]
+	_ = json.NewDecoder(r.Body).Decode(chain)
+	if wallets.Wallets[sender].Token >= token {
+		wallets.Wallets[sender].Token = wallets.Wallets[sender].Token - token
+		wallets.Wallets[recipient].Token = wallets.Wallets[recipient].Token + token
+		chain.AddBlock(token, sender, recipient)
+	}
+	json.NewEncoder(w).Encode(&chain.blocks)
+}
+
+func main() {
+	chain.blocks = append(chain.blocks, Genesis())
+	fmt.Println(chain)
+
+	router := mux.NewRouter()
+	router.HandleFunc("/addwallet", addWallet).Methods("POST")
+	router.HandleFunc("/getAllWalletAddresses", getAllWalletAddresses).Methods("GET")
+	router.HandleFunc("/getAllWalletDetails", getAllWalletDetails).Methods("GET")
+	router.HandleFunc("/transferToken/{val}/{sender}/{recipient}", TransferToken).Methods("POST")
+	http.ListenAndServe(":8000", router)
+}
+
